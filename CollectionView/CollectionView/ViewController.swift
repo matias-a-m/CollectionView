57833b622711426d76d06585c69ada7ba3cf2c28
Matias Molina <matiasmolina@MacBook-Pro-de-Matias.local>
2022-06-11 18:18:34 -0300

    Create Project

---
+++ CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Matias Molina on 11/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CollectionView/readme.md
@@ -0,0 +1,4 @@
+# CollectionView
+
+Help people interested in this repository understand your project by adding a 
+README.
